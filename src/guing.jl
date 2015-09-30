145324099c1fee7af9441b744fca97768471b4e5
Yakir Gagnon <12.yakir@gmail.com>
2015-09-30 18:21:06 +1000

    it's pretty done now.

---
+++ src/guing.jl
@@ -0,0 +1,27 @@
+using Gadfly
+
+Escher.external_setup()
+
+plot_beta(α, β) =
+    plot(x -> x + α* β, 0, 10)
+
+main(window) = begin
+    push!(window.assets, "widgets")
+
+    αᵗ = Input(1.0)
+    βᵗ = Input(1.0)
+
+    vbox(md"## Static Plot",
+        drawing(4Gadfly.inch, 2Gadfly.inch, plot(sin, 0, 25)),
+        md"## Dynamic plot",
+        hbox("Alpha: " |>
+            width(4em), slider(1:100) >>> αᵗ) |>
+            packacross(center),
+        hbox("Beta: "  |>
+            width(4em), slider(1:100) >>> βᵗ) |>
+            packacross(center),
+        lift(αᵗ, βᵗ) do α, β
+            plot_beta(α,β) |> drawing(4Gadfly.inch, 3Gadfly.inch)
+        end
+    ) |> pad(2em)
+end
